b1413b6bea582f0ac2f9083b0d9b5779b883b27b
Ghooney <jediboa@gmail.com>
2026-03-16 18:31:00 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,11 @@
-// 🍀 로또명당지도 Service Worker - v3
-const CACHE_NAME = 'lotto-map-v3';
-
-// 설치 즉시 활성화
+// 🍀 서비스워커 제거 - 기존 등록 해제 및 캐시 삭제
 self.addEventListener('install', () => self.skipWaiting());
-
-// 활성화: 기존 캐시 전부 삭제 후 클라이언트 장악
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys()
       .then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll())
+      .then(clients => clients.forEach(c => c.navigate(c.url)))
   );
 });
-
-// fetch 가로채지 않음 - 브라우저 기본 동작 유지
-// (다운로드 오작동 방지)
