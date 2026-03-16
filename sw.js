247d629d621ba6b8b55240b3e0f6d7c5f21c053d
Ghooney <jediboa@gmail.com>
2026-03-16 18:47:13 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+// 🍀 서비스워커 제거 - 기존 등록 해제 및 캐시 삭제
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll())
+      .then(clients => clients.forEach(c => c.navigate(c.url)))
+  );
+});
